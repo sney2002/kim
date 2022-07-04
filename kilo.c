0b59d59d4fb18bad438d4e59fd62ab8ef96f11d2
Jhonathan Salguero Villa <sney2002@gmail.com>
2022-07-04 12:21:56 -0500

    Move to next line if we are in the last char of a line
    On normal mode we should move to the next line if we move past
    the last character of a line while moving to the right

--- kilo.c
+++ kilo.c
@@ -1269,6 +1269,11 @@ void editorMoveCursor(int key) {
         case ARROW_RIGHT:
             if (row && E.cx < row->size) {
                 E.cx += getCharLen(row->chars, E.cx);
+
+                if (E.mode == NORMAL_MODE && E.cx >= row->size && E.cy < E.numrows - 1) {
+                    E.cy++;
+                    E.cx = 0;
+                }
             } else if (row && E.cx == row->size && E.cy < E.numrows - 1) {
                 E.cy++;
                 E.cx = 0;
@@ -1276,7 +1281,7 @@ void editorMoveCursor(int key) {
         break;
 
         case ARROW_UP:
-            if (E.cy != 0) {
+            if (E.cy > 0) {
                 E.cy--;
                 erow *newrow = E.cy >= E.numrows ? NULL : &E.row[E.cy];
                 if (newrow) {
@@ -1323,10 +1328,12 @@ void editorMoveCursor(int key) {
         break;
     }
 
+    if (E.mode == INSERT_MODE) return;
+
     row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
-    int rowlen = row ? row->size : 0;
-    if (E.cx >= rowlen) {
-        E.cx = E.mode == NORMAL_MODE ? rowlen - 1 : rowlen;
+
+    if (row && E.cx >= row->size) {
+        E.cx = row->size - getPrevCharLen(row->chars, E.cx);
     }
 }
 
